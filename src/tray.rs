6e3deda8149577b655c533590d71b19764eae1cb
Cody Bender <codyfbender@gmail.com>
2026-03-30 12:59:29 -0400

    feat: tray

--- src/tray.rs
+++ src/tray.rs
@@ -8,10 +8,12 @@ pub enum TrayMenuAction {
     Quit,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 pub struct TrayHandle {
     entries: Vec<TrayMenuAction>,
     command_tx: Sender<CoreCommand>,
+    #[cfg(target_os = "linux")]
+    _backend: Option<LinuxTrayBackend>,
 }
 
 impl TrayHandle {
@@ -30,13 +32,81 @@ impl TrayHandle {
 
 #[cfg(target_os = "linux")]
 pub fn create_tray(command_tx: Sender<CoreCommand>) -> Option<TrayHandle> {
+    let backend = LinuxTrayBackend::spawn(command_tx.clone());
     Some(TrayHandle {
         entries: vec![TrayMenuAction::ShowHide, TrayMenuAction::Quit],
         command_tx,
+        _backend: backend,
     })
 }
 
 #[cfg(not(target_os = "linux"))]
 pub fn create_tray(_command_tx: Sender<CoreCommand>) -> Option<TrayHandle> {
     None
+}
+
+#[cfg(target_os = "linux")]
+#[derive(Clone)]
+struct LinuxTrayBackend {
+    _handle: ksni::blocking::Handle<VenturiTray>,
+}
+
+#[cfg(target_os = "linux")]
+impl LinuxTrayBackend {
+    fn spawn(command_tx: Sender<CoreCommand>) -> Option<Self> {
+        use ksni::blocking::TrayMethods;
+
+        let tray = VenturiTray { command_tx };
+        match tray.assume_sni_available(true).spawn() {
+            Ok(handle) => Some(Self { _handle: handle }),
+            Err(error) => {
+                eprintln!("venturi tray backend unavailable: {error}");
+                None
+            }
+        }
+    }
+}
+
+#[cfg(target_os = "linux")]
+#[derive(Debug, Clone)]
+struct VenturiTray {
+    command_tx: Sender<CoreCommand>,
+}
+
+#[cfg(target_os = "linux")]
+impl ksni::Tray for VenturiTray {
+    const MENU_ON_ACTIVATE: bool = true;
+
+    fn id(&self) -> String {
+        "org.venturi.Venturi".to_string()
+    }
+
+    fn title(&self) -> String {
+        "Venturi".to_string()
+    }
+
+    fn icon_name(&self) -> String {
+        "audio-card-symbolic".to_string()
+    }
+
+    fn menu(&self) -> Vec<ksni::MenuItem<Self>> {
+        vec![
+            ksni::menu::StandardItem {
+                label: "Show/Hide".to_string(),
+                activate: Box::new(|tray: &mut Self| {
+                    let _ = tray.command_tx.send(CoreCommand::ToggleWindow);
+                }),
+                ..Default::default()
+            }
+            .into(),
+            ksni::menu::StandardItem {
+                label: "Quit".to_string(),
+                activate: Box::new(|tray: &mut Self| {
+                    let _ = tray.command_tx.send(CoreCommand::Shutdown);
+                }),
+                ..Default::default()
+            }
+            .into(),
+        ]
+    }
 }
